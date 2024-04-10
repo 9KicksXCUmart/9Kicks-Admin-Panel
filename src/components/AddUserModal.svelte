338d3a8033b879aaef0e98d5e40905eb985b72b2
Hillllllllton <honlung1012@gmail.com>
2024-04-10 19:42:52 +0800

    feat: dispaly users & create user

---
+++ src/components/AddUserModal.svelte
@@ -0,0 +1,177 @@
+<!-- AddUserModal.svelte -->
+<script lang="ts">
+	import { PUBLIC_KOTLIN_BACKEND_URL } from '$env/static/public';
+
+	export let showModal: boolean;
+	export let closeModal: () => void;
+	export let data;
+
+	let email = '';
+	let password = '';
+	let firstName = '';
+	let lastName = '';
+	let streetAddress = '';
+	let district = '';
+	let isVerified = false;
+
+	async function createUser() {
+		const newUser = {
+			email,
+			password,
+			firstName,
+			lastName,
+			shippingAddress: {
+				streetAddress,
+				district
+			},
+			isVerified
+		};
+
+		try {
+			const response = await fetch(`${PUBLIC_KOTLIN_BACKEND_URL}api/v1/user-management/create`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${data.jwtToken}`
+				},
+				body: JSON.stringify(newUser)
+			});
+
+			const result = await response.json();
+			console.log('result', result);
+			if (response.ok && result.status) {
+				// Close the modal if the creation was successful
+				closeModal();
+			} else {
+				// Handle any cases where the API response was not ok
+				console.error('Creation failed:', result.message);
+			}
+
+			console.log(result.message);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+</script>
+
+{#if showModal}
+	<div class="modal fixed left-0 top-0 flex h-full w-full items-center justify-center">
+		<div
+			class="modal-overlay absolute h-full w-full bg-gray-900 opacity-50"
+			on:click={closeModal}
+		></div>
+
+		<div
+			class="modal-container z-50 mx-auto w-11/12 overflow-y-auto rounded bg-white shadow-lg md:max-w-md"
+		>
+			<!-- Add margin if you want to see some of the overlay behind the modal-->
+			<div class="modal-content px-6 py-4 text-left">
+				<!--Title-->
+				<div class="flex items-center justify-between pb-3">
+					<p class="text-2xl font-bold">Add New User</p>
+					<div class="modal-close z-50 cursor-pointer" on:click={closeModal}>
+						<span class="text-black">&times;</span>
+					</div>
+				</div>
+
+				<!--Body-->
+				<div class="my-5">
+					<!-- The form fields for adding a new user -->
+					<form on:submit|preventDefault={createUser} class="p-4">
+						<div class="mb-4">
+							<label for="email" class="text-grey-darker mb-2 block text-sm font-bold">Email*</label
+							>
+							<input
+								type="email"
+								id="email"
+								bind:value={email}
+								required
+								class="text-grey-darker w-full appearance-none rounded border px-3 py-2 shadow"
+							/>
+						</div>
+						<div class="mb-4">
+							<label for="password" class="text-grey-darker mb-2 block text-sm font-bold"
+								>Password*</label
+							>
+							<input
+								type="password"
+								id="password"
+								bind:value={password}
+								required
+								class="text-grey-darker w-full appearance-none rounded border px-3 py-2 shadow"
+							/>
+						</div>
+						<div class="mb-4">
+							<label for="firstName" class="text-grey-darker mb-2 block text-sm font-bold"
+								>First Name*</label
+							>
+							<input
+								type="text"
+								id="firstName"
+								bind:value={firstName}
+								required
+								class="text-grey-darker w-full appearance-none rounded border px-3 py-2 shadow"
+							/>
+						</div>
+						<div class="mb-4">
+							<label for="lastName" class="text-grey-darker mb-2 block text-sm font-bold"
+								>Last Name*</label
+							>
+							<input
+								type="text"
+								id="lastName"
+								bind:value={lastName}
+								required
+								class="text-grey-darker w-full appearance-none rounded border px-3 py-2 shadow"
+							/>
+						</div>
+						<!-- ... other input fields ... -->
+						<div class="mb-4">
+							<label for="streetAddress" class="text-grey-darker mb-2 block text-sm font-bold"
+								>Street Address*</label
+							>
+							<input
+								type="text"
+								id="streetAddress"
+								bind:value={streetAddress}
+								required
+								class="text-grey-darker w-full appearance-none rounded border px-3 py-2 shadow"
+							/>
+						</div>
+						<div class="mb-4">
+							<label for="district" class="text-grey-darker mb-2 block text-sm font-bold"
+								>District*</label
+							>
+							<input
+								type="text"
+								id="district"
+								bind:value={district}
+								required
+								class="text-grey-darker w-full appearance-none rounded border px-3 py-2 shadow"
+							/>
+						</div>
+						<button
+							type="submit"
+							class="rounded-lg bg-blue-500 p-3 px-4 text-white hover:bg-blue-400"
+						>
+							Submit
+						</button>
+						<!-- ... submit button ... -->
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
+
+<style>
+	.modal {
+		transition: opacity 0.25s ease;
+	}
+	.modal-overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 40;
+	}
+</style>
--- src/routes/admin/user/+page.svelte
+++ src/routes/admin/user/+page.svelte
@@ -1,9 +1,95 @@
-<script>
+<script lang="ts">
 	import Content from '../../../components/Content.svelte';
+	import { onMount } from 'svelte';
+	import { PUBLIC_KOTLIN_BACKEND_URL } from '$env/static/public';
+	import AddUserModal from '../../../components/AddUserModal.svelte';
+	export let data;
+
+	let showAddUserModal = false;
+	function toggleAddUserModal() {
+		showAddUserModal = !showAddUserModal;
+	}
+	interface userInfo {
+		id: string;
+		email: string;
+		password: string;
+		firstname: string;
+		lastname: string;
+		shippingaddress: string[];
+		isVerified: boolean;
+	}
+	let users: userInfo[] = [];
+
+	async function getAllUsers() {
+		try {
+			const response = await fetch(`${PUBLIC_KOTLIN_BACKEND_URL}api/v1/user-management/users`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${data.jwtToken}`
+				}
+			});
+
+			const result = await response.json();
+
+			// Make sure you are accessing the correct path in your JSON structure
+			return result.data;
+		} catch (error) {
+			console.error('Failed to fetch users:', error);
+			return []; // Return an empty array in case of error to ensure users is always an array
+		}
+	}
+	onMount(async () => {
+		users = await getAllUsers();
+	});
 </script>
 
-<svelte:head>
-	<title>Projects</title>
-</svelte:head>
-
-<Content title="User Management" />
+<div class="mb-4 flex items-center justify-between py-5">
+	<div class="mb-4 py-5 text-center text-3xl font-bold">Users Management</div>
+	<button
+		class="rounded bg-gray-200 px-4 py-2 font-bold text-black hover:bg-gray-500"
+		on:click={toggleAddUserModal}
+	>
+		+
+	</button>
+	<AddUserModal showModal={showAddUserModal} closeModal={toggleAddUserModal} {data} />
+</div>
+<table class="table-auto">
+	<thead>
+		<tr class="bg-gray-200 text-sm leading-normal text-gray-600">
+			<th class="px-6 py-3">Email</th>
+			<th class="px-6 py-3">FirstName</th>
+			<th class="px-6 py-3">LastName</th>
+			<th class="px-6 py-3">Shipping Address</th>
+			<th class="px-6 py-3">Status</th>
+			<th class="px-6 py-3">Edit</th>
+		</tr>
+	</thead>
+	<tbody class="text-sm font-light text-gray-600">
+		{#each users as user}
+			<tr class="cursor-pointer border-b border-gray-200 hover:bg-gray-100">
+				<td class="px-6 py-3">{user.email}</td>
+				<td class="px-6 py-3">{user.firstName}</td>
+				<td class="px-6 py-3">{user.lastName}</td>
+				<td class="px-6 py-3">
+					{user.shippingAddress.streetAddress}
+					{user.shippingAddress.district}
+				</td>
+				<td>
+					<span
+						class="status-circle relative inline-block px-3 py-1 text-sm font-semibold leading-tight"
+					>
+						<span
+							aria-hidden="true"
+							class="absolute inset-0 rounded-full opacity-50 {user.isVerified
+								? 'bg-green-200'
+								: 'bg-red-200'}"
+						></span>
+						<span class="relative">{user.isVerified ? 'active' : 'inactive'}</span>
+					</span>
+				</td>
+				<td class="px-6 py-3">Edit</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
