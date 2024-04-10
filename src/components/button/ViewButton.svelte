e6f6815b8f252b84c086ddaef670785cebd906a8
felixfung203 <103751855+felixfung203@users.noreply.github.com>
2024-04-10 20:15:50 +0800

    feat: finish product management (#5)
    * chore: update favicon
    
    * feat: create basic product table component
    
    * feat: integrate product api
    
    * feat: finish create product api
    
    ---------
    
    Co-authored-by: nickwkt <nickb55245@gmail.com>

---
+++ src/components/button/ViewButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function handleClick() {
+		dispatch('click');
+	}
+</script>
+
+<button on:click={handleClick}>
+	<svg
+		class="h-6 w-6 text-gray-800 dark:text-white"
+		aria-hidden="true"
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<path
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-width="2"
+			d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"
+		/>
+	</svg>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,18 @@
 </svelte:head>
 
 <div class="flex flex-row gap-20 text-2xl font-bold">
-
 	<a href="/admin/user" class="group text-sky-600 transition duration-300">
 		User Management
-		<span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+		<span class="block h-0.5 max-w-0 bg-sky-600 transition-all duration-500 group-hover:max-w-full"
+		></span>
 	</a>
 
-	<a href="/admin/product" class="group text-sky-600 transition duration-300">
+	<a href="/admin/product1" class="group text-sky-600 transition duration-300">
 		Product Management
-		<span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+		<span class="block h-0.5 max-w-0 bg-sky-600 transition-all duration-500 group-hover:max-w-full"
+		></span>
 	</a>
-
 </div>
 
 <style>
-
 </style>
